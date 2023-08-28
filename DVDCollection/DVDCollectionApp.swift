c6e2426aa20ef7efc3c3569ea885eceb98fcc8bd
Samuel <92946129+Cravatec@users.noreply.github.com>
2023-08-28 14:54:40 +0200

    Initial Commit

---
+++ DVDCollection/DVDCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVDCollectionApp.swift
+//  DVDCollection
+//
+//  Created by Sam on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DVDCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
