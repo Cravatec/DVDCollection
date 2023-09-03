f62e93bc148fa980057e47c531faf8100e795f5b
Samuel <92946129+Cravatec@users.noreply.github.com>
2023-09-03 12:07:32 +0300

    add Stars in DVDDetailView and add a sortedStars for having Director in first

--- DVDCollection/DetailView.swift
+++ DVDCollection/DetailView.swift
@@ -31,7 +31,30 @@ struct DVDDetailView: View {
                 .font(.footnote)}
             Text("Cover: \(dvd.cover)")
                 .font(.footnote)
+            VStack(alignment: .leading) {
+                Text("Stars:")
+                    .font(.headline)
+                let sortedStars = dvd.stars.star.sorted { star1, star2 in
+                    if star1.type == .réalisateur {
+                        return true
+                    } else if star2.type == .réalisateur {
+                        return false
+                    } else {
+                        return star1.text < star2.text
+                    }
+                }
+                ForEach(sortedStars, id: \.identifier) { star in
+                    HStack {
+                        Text("\(star.type.rawValue):")
+                            .font(.subheadline)
+                        Text(star.text)
+                            .font(.subheadline)
+                    }
+                }
+            }
+
         }
+        
         .padding()
         .navigationBarTitle(dvd.titres.fr)
     }
@@ -39,6 +62,6 @@ struct DVDDetailView: View {
 
 struct DVDDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DVDDetailView(dvd: Dvd(id: "12", media: "DVD", cover: "", titres: Titres.init(fr: "Les Azerty en été", vo: "The Qwerty", alternatif: "", alternatifVo: ""), annee: "2023", edition: "Swift", editeur: "Apple"))
+        DVDDetailView(dvd: Dvd(id: "12", media: "DVD", cover: "", titres: Titres.init(fr: "Les Azerty en été", vo: "The Qwerty", alternatif: "", alternatifVo: ""), annee: "2023", edition: "Swift", editeur: "Apple", stars: Stars(star: [Star(type: .acteur, id: "22", text: "Mr Machin")])))
     }
 }
