9d2bf68d49d07199e25281294276ebb05ec9e265
Samuel <92946129+Cravatec@users.noreply.github.com>
2023-09-09 11:37:14 +0200

    UI update in DVDListView

--- DVDCollection/ListView.swift
+++ DVDCollection/ListView.swift
@@ -18,31 +18,54 @@ struct DVDListView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                SearchBar(text: $searchText, placeholder: "Search DVDs")
-                
-                ScrollView {
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
-                        ForEach(searchText.isEmpty ? viewModel.dvds : viewModel.filteredDvds(searchText: searchText), id: \.id) { dvd in
-                            NavigationLink(destination: DVDDetailView(dvd: dvd)) {
-                                DVDGridItem(dvd: dvd)
+            VStack(){
+                HStack {
+                    Text("DVD Collection")
+                        .font(.body)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Button {
+                        isShowingScanner = true
+                    } label: {
+                        Label("Scan", systemImage: "barcode.viewfinder")
+                            .sheet(isPresented: $isShowingScanner) {
+                                CodeScannerView(codeTypes: [.ean13], showViewfinder: true, simulatedData: simulatedBarcode.randomElement()!, shouldVibrateOnSuccess: true, completion: handleScan)
+                            }
+                    }
+                }
+                VStack() {
+                    SearchBar(text: $searchText, placeholder: "Search DVDs")
+                    
+                    ScrollView {
+                        LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
+                            ForEach(searchText.isEmpty ? viewModel.dvds : viewModel.filteredDvds(searchText: searchText), id: \.id) { dvd in
+                                NavigationLink(destination: DVDDetailView(dvd: dvd)) {
+                                    DVDGridItem(dvd: dvd)
+                                }
                             }
                         }
+                        .padding()
                     }
-                    .padding()
                 }
-            }
-            .alert(isPresented: $scannerDispatcher.isShowingMessage) {
-                Alert(title: Text("This Media is already in your collection"), message: Text(scannerDispatcher.message), dismissButton: .default(Text("OK")))}
-            .navigationBarTitle("DVD Collection")
-            .toolbar { Button {
-                isShowingScanner = true
-            } label: {
-                Label("Scan", systemImage: "barcode.viewfinder")
-                    .sheet(isPresented: $isShowingScanner) {
-                        CodeScannerView(codeTypes: [.ean13], showViewfinder: true, simulatedData: simulatedBarcode.randomElement()!, shouldVibrateOnSuccess: true, completion: handleScan)
-                    }
-            }
+                .alert(isPresented: $scannerDispatcher.isShowingMessage) {
+                    Alert(title: Text("⚠️ Attention ⚠️"), message: Text(scannerDispatcher.message), dismissButton: .default(Text("OK")))}
+                //  .navigationBarTitle("DVD Collection")
+                .toolbar {
+                    //                HStack {
+                    //                    Text("DVD Collection")
+                    //                        .font(.body)
+                    //                        .fontWeight(.bold)
+                    //                    Spacer()
+                    //                    Button {
+                    //                        isShowingScanner = true
+                    //                    } label: {
+                    //                        Label("Scan", systemImage: "barcode.viewfinder")
+                    //                            .sheet(isPresented: $isShowingScanner) {
+                    //                                CodeScannerView(codeTypes: [.ean13], showViewfinder: true, simulatedData: simulatedBarcode.randomElement()!, shouldVibrateOnSuccess: true, completion: handleScan)
+                    //                            }
+                    //                    }
+                    //                }
+                }
             }
         }
         .padding()
@@ -52,7 +75,7 @@ struct DVDListView: View {
         }
     }
     
-    let simulatedBarcode = ["3760137632648", "5051889638940", "3700301045065", "5051889675693", "3333290005415", "5053083261993", "3701432014517", "3701432006000"]
+    let simulatedBarcode = ["3760137632648", "5051889638940", "3700301045065", "5051889675693", "3333290005415", "5053083261993", "3701432014517", "3701432006000", "5051889700371", "5051889638957"]
     
     func handleScan(result: Result<ScanResult, ScanError>) {
         isShowingScanner = false
@@ -72,60 +95,58 @@ struct DVDListView: View {
         }
     }
 }
-    struct DVDGridItem: View {
-        let dvd: Dvd
-
-        var body: some View {
-            HStack {
-                VStack {
-                    Image(systemName: "film").imageScale(.large)
-                    Text(dvd.media)
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                }
-                VStack(alignment: .leading) {
-                    Text(dvd.titres.fr)
-                        .font(.headline)
-                    Text(dvd.titres.vo)
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                        .lineLimit(1)
-                        .padding(.trailing)
-                    Text(dvd.annee)
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
-                    Text(dvd.edition).font(.subheadline)
-                        .foregroundColor(.gray)
-                }
+struct DVDGridItem: View {
+    let dvd: Dvd
+    
+    var body: some View {
+        HStack {
+            VStack {
+                Image(systemName: "film").imageScale(.large)
+                Text(dvd.media)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            VStack(alignment: .leading) {
+                Text(dvd.titres.fr)
+                    .font(.headline)
+                Text(dvd.titres.vo)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .lineLimit(1)
+                    .padding(.trailing)
+                Text(dvd.annee)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Text(dvd.edition).font(.subheadline)
+                    .foregroundColor(.gray)
             }
         }
     }
+}
 
-    struct SearchBar: View {
-        @Binding var text: String
-        var placeholder: String
-        
-        var body: some View {
-            HStack {
-                TextField(placeholder, text: $text)
-                    .padding(8)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .padding([.leading, .trailing], 4)
-                    .padding(.top, 8)
-                    .padding(.bottom, 4)
-                Button(action: {
-                    text = ""
-                }) {
-                    Image(systemName: "xmark.circle.fill")
-                        .foregroundColor(.gray)
-                        .padding(4)
-                }
-                .padding(.trailing, 8)
-                .opacity(text.isEmpty ? 0 : 1)
+struct SearchBar: View {
+    @Binding var text: String
+    var placeholder: String
+    
+    var body: some View {
+        HStack {
+            TextField(placeholder, text: $text)
+                .padding(8)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .padding([.leading, .trailing], 4)
+                .padding(.top, 8)
+                .padding(.bottom, 4)
+            Button(action: { text = "" }) {
+                Image(systemName: "xmark.circle.fill")
+                    .foregroundColor(.gray)
+                    .padding(4)
             }
+            .padding(.trailing, 8)
+            .opacity(text.isEmpty ? 0 : 1)
         }
     }
+}
 
 class DVDListViewModel: ObservableObject {
     @Published var dvds: [Dvd] = []
@@ -142,27 +163,39 @@ class DVDListViewModel: ObservableObject {
             }
         }
     }
+    
     func filteredDvds(searchText: String) -> [Dvd] {
-            if searchText.isEmpty {
-                return dvds // Return all DVDs if searchText is empty
-            } else {
-                // Filter DVDs based on the searchText
-                return dvds.filter { $0.titres.fr.localizedCaseInsensitiveContains(searchText) }
-            }
+        if searchText.isEmpty {
+            return dvds // Return all DVDs if searchText is empty
+        } else {
+            // Filter DVDs based on the searchText
+            return dvds.filter { $0.titres.fr.localizedCaseInsensitiveContains(searchText) }
         }
+    }
 }
 
 struct DVDListView_Previews: PreviewProvider {
     static var previews: some View {
-        let viewModel = DVDListViewModel()
-        viewModel.dvds = [
-            Dvd(id: "123", media: "DVD", cover: "image", titres: Titres(fr: "Faux Titre DVD", vo: "Fake DVD Title", alternatif: "Titre Alternatif", alternatifVo: "Alternate Title"), annee: "2023", edition: "Édition THX", editeur: "Criterion", stars: Stars(star: [
-                                Star(type: .acteur, id: "1", text: "Patrick Sebastion"),
-                                Star(type: .acteur, id: "2", text: "Jean Reno"),
-                                Star(type: .réalisateur, id: "3", text: "Stanley Kubrick")
-                            ]), barcode: "5051889638940")
-        ]
-        return DVDListView()
-            .environmentObject(viewModel)
+//        let viewModel = DVDListViewModel()
+//        viewModel.dvds = [
+//Dvd(id: "123", media: "DVD", cover: "image", titres: Titres(fr: "Faux Titre DVD", vo: "Fake DVD Title", alternatif: "Titre Alternatif", alternatifVo: "Alternate Title"), annee: "2023", edition: "Édition THX", editeur: "Criterion", stars: Stars(star: [
+//                Star(type: .acteur, id: "1", text: "Patrick Sebastion"),
+//                Star(type: .acteur, id: "2", text: "Jean Reno"),
+//                Star(type: .réalisateur, id: "3", text: "Stanley Kubrick")
+//            ]), barcode: "5051889638940"),
+//            Dvd(id: "231", media: "DVD", cover: "image", titres: Titres(fr: "Faux Titre DVD 2", vo: "Fake DVD Title 2", alternatif: "Titre Alternatif 2", alternatifVo: "Alternate Title 2"), annee: "1928", edition: "Édition VHS", editeur: "Criterion", stars: Stars(star: [
+//                Star(type: .acteur, id: "1", text: "Patrick Sebastion"),
+//                Star(type: .acteur, id: "2", text: "Jean Reno"),
+//                Star(type: .réalisateur, id: "3", text: "Stanley Kubrick")
+//            ]), barcode: "3701432014517")]
+//        return DVDListView().environmentObject(viewModel)
+        
+//        DVDGridItem(dvd: Dvd(id: "123", media: "DVD", cover: "cover", titres: Titres(fr: "Hello", vo: "Hella", alternatif: "Toto", alternatifVo: "Titi"), annee: "2023", edition: "THX", editeur: "Criterion", stars: Stars(star: [
+//                        Star(type: .acteur, id: "1", text: "Patrick Sebastion"),
+//                        Star(type: .acteur, id: "2", text: "Jean Reno"),
+//                        Star(type: .réalisateur, id: "3", text: "Stanley Kubrick")
+//                    ]), barcode: "12344323"))
+        
+        DVDListView()
     }
 }
