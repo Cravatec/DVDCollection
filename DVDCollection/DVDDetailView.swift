ead0ce7bc55a6b5ad7edfbcda0c2cdc678d4efb4
Samuel <92946129+Cravatec@users.noreply.github.com>
2023-09-12 13:22:02 +0200

    UI update in DVDDetailView

--- DVDCollection/DVDDetailView.swift
+++ DVDCollection/DVDDetailView.swift
@@ -14,7 +14,8 @@ struct DVDDetailView: View {
         ScrollView(showsIndicators: true) {
             VStack{
                 VStack {
-                    if let data = dvd.coverImageData, let uiImage = UIImage(data: data) {
+                    if let data = dvd.coverImageData,
+                       let uiImage = UIImage(data: data) {
                         Image(uiImage: uiImage)
                             .resizable()
                             .aspectRatio(contentMode: .fit)
@@ -31,14 +32,14 @@ struct DVDDetailView: View {
                                                height: 40)
                                         .background(Color.white)
                                         .cornerRadius(30)
-                                        .position(x: media.size.width * 0.9, y: media.size.height * 0.95)
+                                        .position(x: media.size.width * 0.85, y: media.size.height * 0.95)
                                 }
                             )
                     } else {
                         Image(systemName: "film")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(height: 250,
+                            .frame(width: 160, height: 250,
                                    alignment: .bottom)
                             .clipped()
                             .overlay(
@@ -51,57 +52,78 @@ struct DVDDetailView: View {
                                                height: 40)
                                         .background(Color.white)
                                         .cornerRadius(30)
-                                        .position(x: media.size.width * 0.9, y: media.size.height * 0.95)
+                                        .position(x: media.size.width * 0.85, y: media.size.height * 0.95)
                                 }
                             )
                     }
                 }.background(Color.white)
+                    .frame(height: 280, alignment: .center)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.gray, lineWidth: 1)
+                    )
+//                .shadow(color: Color.gray.opacity(0.3), radius: 20, x: 0, y: 10)
+//                .shadow(color: Color.gray.opacity(0.2), radius: 5, x: 0, y: 2)
                 VStack{
                     VStack {
                         Text("\(dvd.titres.fr)")
-                            .font(.title3)
+                            .font(.title3).fontWeight(.bold)
                         Text("Original Title: \(dvd.titres.vo)").font(.footnote).padding(.horizontal).opacity(dvd.titres.vo.isEmpty ? 0 : 1)
-                    }.allowsTightening(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-                        .frame(maxWidth: .greatestFiniteMagnitude, maxHeight: .greatestFiniteMagnitude, alignment: .center)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color.white)
+                    }
+                    .allowsTightening(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                    .frame(maxWidth: .greatestFiniteMagnitude,
+                           maxHeight: .greatestFiniteMagnitude,
+                           alignment: .center)
+                    .cornerRadius(10)
+                    .multilineTextAlignment(.center)
+                    .shadow(radius: 20)
+                    
                     HStack {
                         Text("Alternative Title: \(dvd.titres.alternatif)\(dvd.titres.alternatifVo)").opacity(dvd.titres.alternatif.isEmpty && dvd.titres.alternatifVo.isEmpty ? 0 : 1)
-                    }.font(.caption)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color.white)
+                    }
+                    .font(.caption)
+                    .multilineTextAlignment(.center)
+                    
                     HStack {
                         Text("Year: \(dvd.annee)").opacity(dvd.annee.isEmpty ? 0 : 1)
                         Image(systemName: "barcode")
                         Text(": \(dvd.barcode)")
-                    }.font(.footnote).frame(width: 500, height: 10, alignment: .center).foregroundColor(Color.white)
+                    }
+                    .font(.footnote)
+                    .frame(width: 500,
+                           height: 10,
+                           alignment: .center)
+                    
                     VStack {
                         Text("Editor: \(dvd.editeur)")
                         Text("Edition: \(dvd.edition)").opacity(dvd.edition.isEmpty ? 0 : 1)
-                    }.font(.footnote).foregroundColor(Color.white)
-                    VStack(alignment: .leading) {
-                        Text("Stars:")
-                            .font(.headline)
-                        let sortedStars = dvd.stars.star.sorted { star1, star2 in
-                            if star1.type == .réalisateur {
-                                return true
-                            } else if star2.type == .réalisateur {
-                                return false
-                            } else {
-                                return star1.text < star2.text
-                            }
+                    }
+                    .font(.footnote)
+                }
+                VStack(alignment: .leading) {
+                    Text("Stars:")
+                        .font(.headline)
+                    let sortedStars = dvd.stars.star.sorted
+                    { star1, star2 in
+                        if star1.type == .réalisateur {
+                            return true
+                        } else if star2.type == .réalisateur {
+                            return false
+                        } else {
+                            return star1.text < star2.text
                         }
-                        ForEach(sortedStars, id: \.identifier) { star in
-                            HStack {
-                                Text("\(star.type.rawValue):")
-                                    .font(.body)
-                                Text(star.text)
-                                    .font(.body)
-                                    .fontWeight(.semibold)
-                            }
+                    }
+                    ForEach(sortedStars, id: \.identifier) { star in
+                        HStack {
+                            Text("\(star.type.rawValue):")
+                                .font(.body)
+                            Spacer()
+                            Text(star.text)
+                                .font(.body)
+                                .fontWeight(.semibold)
                         }
-                    }.foregroundColor(Color.white)
-                }.background(Color.black)
+                    }
+                }
             }.frame(minWidth: 200,
                     maxWidth: .greatestFiniteMagnitude,
                     minHeight: 200,
@@ -109,7 +131,7 @@ struct DVDDetailView: View {
                     alignment:.center)
             .padding()
             .navigationBarTitle(dvd.titres.fr)
-        }.background(Color.white)
+        }
     }
 }
 
